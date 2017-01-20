6557e9e863668d63307753309168844ced60564e
Shaun <shaunremekie@Shauns-MacBook-Pro.local>
2017-01-20 15:46:15 +0000

    Updated translate.go with request handling code

--- translate.go
+++ translate.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	cli "github.com/jawher/mow.cli"
-	"github.com/olekukonko/tablewriter"
-
 	"cloud.google.com/go/translate"
+	"github.com/olekukonko/tablewriter"
 	"golang.org/x/text/language"
 )
 
@@ -18,19 +16,11 @@ type request struct {
 	codes  map[string]string // Language code map
 }
 
-const usage string = `Usage: translate [OPTIONS]
-
-Simple app for translating text via Google Translate API
-
-Options:
-  -l, --language-code=""      Desired ouput Language Code
-  -t, --text=""               The text to be translated
-  --codes                     Lists some available language codes
-`
-
 func (r *request) getCodes() {
 	// Sets code property with map of available language codes
-	m := map[string]string{
+
+	// set codes property in struct
+	r.codes = map[string]string{
 		"Spanish":  "es",
 		"Greek":    "el",
 		"Japanese": "ja",
@@ -41,9 +31,6 @@ func (r *request) getCodes() {
 		"Italian":  "it",
 		"Swedish":  "sv",
 	}
-
-	// set codes property in struct
-	r.codes = m
 }
 
 func (r *request) printCodes() {
@@ -64,7 +51,7 @@ func (r *request) printCodes() {
 	fmt.Printf("\n" + `See https://ctrlq.org/code/19899-google-translate-languages for details on other available codes` + "\n\n")
 }
 
-func (r *request) translateText() (string, error) {
+func (r *request) Translate() (string, error) {
 	// Returns translate text string via Google Translate API
 	ctx := context.Background()
 
@@ -85,49 +72,4 @@ func (r *request) translateText() (string, error) {
 		return "", err
 	}
 	return resp[0].Text, nil
-}
-
-func main() {
-	// Defining CLI options in main function
-	args := make(map[string]*string)
-	app := cli.App("translate", "Simple app for translating text via Google Translate API")
-	args["language-code"] = app.StringOpt("l language-code", "", `Desired ouput Language Code`)
-	args["text"] = app.StringOpt("t text", "", `The text to be translated`)
-	codesFlag := app.BoolOpt("codes", false, "Lists some available language codes")
-
-	app.Cmd.Action = func() {
-
-		if *codesFlag {
-			req := request{}
-			req.printCodes()
-			return
-		}
-
-		if *args["language-code"] == "" && *args["text"] == "" {
-			fmt.Println(usage)
-			return
-		}
-
-		req := request{
-			target: *args["language-code"],
-			text:   *args["text"],
-		}
-
-		for k, v := range args {
-			if *v == "" {
-				fmt.Printf(`Please specify "%s"`+"\n", k)
-				return
-			}
-		}
-
-		resp, err := req.translateText()
-		if err != nil {
-			fmt.Println("ERROR:", err.Error())
-		} else {
-			fmt.Println(resp)
-		}
-
-	}
-
-	app.Run(os.Args)
 }
