79fe771004d067b0691abec458ca8fd6d8be5aee
Shaun Remekie <daidokoro@macbook-pro.mynet>
2020-04-29 12:03:58 +0100

    added language-code flag

--- commands.go
+++ commands.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-var codes bool
+var (
+	codes    bool   // used as language code listing flag
+	langCode string // used as language code selection flag
+)
+
+// Cobra Init function.
+func init() {
+	rootCmd.Flags().BoolVarP(&codes, "codes", "C", false, "List of available Language Codes")
+	rootCmd.Flags().StringVarP(&langCode, "language-code", "c", "en", "Select language code")
+}
 
 // root command
 var rootCmd = &cobra.Command{
-	Use:   `translate ["Lang Code:Text"]`,
+	Use:   `translate -c [Lang Code] [Text]`,
 	Short: "Simple app for translating text via Google Translate API ",
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -21,16 +29,16 @@ var rootCmd = &cobra.Command{
 			return
 		}
 
-		if len(args) < 1 || !strings.Contains(args[0], ":") {
+		if len(args) < 1 {
 			cmd.Help()
 			return
 		}
 
-		t := strings.Split(args[0], ":")
+		t := args[0]
 
 		req := &request{
-			target: strings.TrimSpace(t[0]),
-			text:   strings.TrimSpace(t[1]),
+			target: langCode,
+			text:   t,
 		}
 
 		resp, err := req.Translate()
@@ -41,9 +49,4 @@ var rootCmd = &cobra.Command{
 		}
 
 	},
-}
-
-// Cobra Init function.
-func init() {
-	rootCmd.Flags().BoolVarP(&codes, "codes", "c", false, "List of available Language Codes")
 }
